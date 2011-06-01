d822c66a342bafe2f2a46d4af6033c466289fe22
erik15 <kavas.erik@gmail.com>
2011-06-01 12:56:13 +0200

    Verzija 1.1
    Dodani dnevi za sajenje

---
+++ Vrtnar/src/edu/erik/vrtnar/android/DneviTeden.java
@@ -0,0 +1,66 @@
+package edu.erik.vrtnar.android;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Toast;
+
+public class DneviTeden extends Activity implements OnClickListener {
+	public static final String sPON="PON";
+	public static final String sTOR="TOR";
+	public static final String sSRE="SRE";
+	public static final String sCET="CET";
+	public static final String sPET="PET";
+	public static final String sSOB="SOB";
+	public static final String sNED="NED";
+	CheckBox cbPon;
+	CheckBox cbTor;
+	CheckBox cbSre;
+	CheckBox cbCet;
+	CheckBox cbPet;
+	CheckBox cbSob;
+	CheckBox cbNed;
+	
+	
+	Button shrani;
+	Button dnevi_sajenja;
+	Menu meni;
+	
+	public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tedendnevi);
+        cbPon = (CheckBox)  findViewById(R.id.cb_dsajenja_pon);
+        cbTor = (CheckBox)  findViewById(R.id.cb_dsajenja_tor);
+        cbSre = (CheckBox)  findViewById(R.id.cb_dsajenja_sre);
+        cbCet = (CheckBox)  findViewById(R.id.cb_dsajenja_cet);
+        cbPet = (CheckBox)  findViewById(R.id.cb_dsajenja_pet);
+        cbSob = (CheckBox)  findViewById(R.id.cb_dsajenja_sob);
+        cbNed = (CheckBox)  findViewById(R.id.cb_dsajenja_ned);
+        shrani = (Button) findViewById(R.id.btn_shrani);
+        shrani.setOnClickListener(this);
+      
+	}
+
+	@Override
+	public void onClick(View v) {
+		Intent intent = new Intent();
+		intent.putExtra(sPON, cbPon.isChecked()); //shrani vrednosti v intent
+		intent.putExtra(sTOR, cbTor.isChecked());
+		intent.putExtra(sSRE, cbSre.isChecked());
+		intent.putExtra(sCET, cbCet.isChecked());
+		intent.putExtra(sPET, cbPet.isChecked());
+		intent.putExtra(sSOB, cbSob.isChecked());
+		intent.putExtra(sNED, cbNed.isChecked());
+		
+		setResult(RESULT_OK,intent);
+	    finish();	
+	}
+	
+}
--- Vrtnar/src/edu/erik/vrtnar/android/Dodaj_Okno.java
+++ Vrtnar/src/edu/erik/vrtnar/android/Dodaj_Okno.java
@@ -13,26 +13,59 @@ import android.widget.Toast;
 
 public class Dodaj_Okno extends Activity implements OnClickListener {
 
-	Button shrani;
+	//Button shrani;
 	Button dnevi_sajenja;
 	Menu meni;
+	public static final int OPEN_DNEVI=99;
 	
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.drugi);
         
-        shrani = (Button) findViewById(R.id.btn_shrani);
-        shrani.setOnClickListener(this);
+        //shrani = (Button) findViewById(R.id.btn_shrani);
+        //shrani.setOnClickListener(this);
         dnevi_sajenja = (Button) findViewById(R.id.btn_dsajenja);
         dnevi_sajenja.setOnClickListener(this);
 	}
 
 	@Override
 	public void onClick(View v) {
-		// TODO Auto-generated method stub
+		Intent i = new Intent();
+		i.setClass(this, DneviTeden.class);
+		startActivityForResult(i, OPEN_DNEVI);
 		
 	}
-	
+	 @Override
+	 protected void onActivityResult (int requestCode, int resultCode, Intent data) {
+		 if (resultCode==RESULT_OK) {
+			 if (requestCode==OPEN_DNEVI) {
+				 String izbrani="";
+				 if (data.getBooleanExtra(DneviTeden.sPON, false)) { //neki naredis
+					 izbrani +="PON "; 
+				 }
+				 if (data.getBooleanExtra(DneviTeden.sTOR, false)) { //neki naredis
+					 izbrani +="TOR "; 
+				 }
+				 if (data.getBooleanExtra(DneviTeden.sSRE, false)) { //neki naredis
+					 izbrani +="SRE "; 
+				 }
+				 if (data.getBooleanExtra(DneviTeden.sCET, false)) { //neki naredis
+					 izbrani +="ÈET"; 
+				 }
+				 if (data.getBooleanExtra(DneviTeden.sPET, false)) { //neki naredis
+					 izbrani +="PET "; 
+				 }
+				 if (data.getBooleanExtra(DneviTeden.sSOB, false)) { //neki naredis
+					 izbrani +="SOB "; 
+				 }
+				 if (data.getBooleanExtra(DneviTeden.sNED, false)) { //neki naredis
+					 izbrani +="NET "; 
+				 }
+				 Toast.makeText(this, izbrani, Toast.LENGTH_SHORT)
+					.show(); 
+			 }
+		 }
+	 }
     @Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		meni = menu;
