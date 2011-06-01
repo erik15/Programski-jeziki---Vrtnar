3eda70d70f86c5706f772bf629c4938238b8f61e
erik15 <kavas.erik@gmail.com>
2011-06-01 13:34:10 +0200

    Verzija 1.1
    Dodani dnevi za zalivanje

---
+++ Vrtnar/src/edu/erik/vrtnar/android/DneviZalivanja.java
@@ -0,0 +1,66 @@
+package edu.erik.vrtnar.android;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Toast;
+
+public class DneviZalivanja extends Activity implements OnClickListener {
+	public static final String zPON="PON";
+	public static final String zTOR="TOR";
+	public static final String zSRE="SRE";
+	public static final String zCET="CET";
+	public static final String zPET="PET";
+	public static final String zSOB="SOB";
+	public static final String zNED="NED";
+	CheckBox cbPon;
+	CheckBox cbTor;
+	CheckBox cbSre;
+	CheckBox cbCet;
+	CheckBox cbPet;
+	CheckBox cbSob;
+	CheckBox cbNed;
+	
+	
+	Button shrani_zalivanje;
+	Button dnevi_zalivanja;
+	Menu meni;
+	
+	public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tedendnevi);
+        cbPon = (CheckBox)  findViewById(R.id.cb_dzalivanja_pon);
+        cbTor = (CheckBox)  findViewById(R.id.cb_dzalivanja_tor);
+        cbSre = (CheckBox)  findViewById(R.id.cb_dzalivanja_sre);
+        cbCet = (CheckBox)  findViewById(R.id.cb_dzalivanja_cet);
+        cbPet = (CheckBox)  findViewById(R.id.cb_dzalivanja_pet);
+        cbSob = (CheckBox)  findViewById(R.id.cb_dzalivanja_sob);
+        cbNed = (CheckBox)  findViewById(R.id.cb_dzalivanja_ned);
+        shrani_zalivanje = (Button) findViewById(R.id.btn_shrani_zalivanje);
+        shrani_zalivanje.setOnClickListener(this);
+      
+	}
+
+	@Override
+	public void onClick(View v) {
+		Intent intent = new Intent();
+		intent.putExtra(zPON, cbPon.isChecked()); //shrani vrednosti v intent
+		intent.putExtra(zTOR, cbTor.isChecked());
+		intent.putExtra(zSRE, cbSre.isChecked());
+		intent.putExtra(zCET, cbCet.isChecked());
+		intent.putExtra(zPET, cbPet.isChecked());
+		intent.putExtra(zSOB, cbSob.isChecked());
+		intent.putExtra(zNED, cbNed.isChecked());
+		
+		setResult(RESULT_OK,intent);
+	    finish();	
+	}
+	
+}
--- Vrtnar/src/edu/erik/vrtnar/android/vrtnar.java
+++ Vrtnar/src/edu/erik/vrtnar/android/vrtnar.java
@@ -1,24 +1,29 @@
 package edu.erik.vrtnar.android;
 
 import java.io.InputStream;
-import java.net.URI;
 import java.net.URL;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
-import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class vrtnar extends Activity implements OnClickListener {
 	/** Called when the activity is first created. */
-	private static final int TEST_START_ACTIVITY_ID = 1;
-	Button dodaj, doloci_dneve;
+	
+	Button dodaj, doloci_dneve_zalivanja;
 	ImageView vreme;
+	Menu meni;
+	private static final int TEST_START_ACTIVITY_ID = 1;
+	private static final int OPEN_DNEVI_ZALIVANJA = 2;
 	
 	
 	@Override
@@ -28,13 +33,16 @@ public class vrtnar extends Activity implements OnClickListener {
 
 		dodaj = (Button) findViewById(R.id.btn_dodaj);
 		dodaj.setOnClickListener(this);
-		doloci_dneve = (Button) findViewById(R.id.btn_doloci);
-		doloci_dneve.setOnClickListener(this);
+		
+		doloci_dneve_zalivanja = (Button) findViewById(R.id.btn_dzalivanja);
+		doloci_dneve_zalivanja.setOnClickListener(this);
+		
 		vreme = (ImageView) findViewById(R.id.vreme_slika);
 		Drawable drawable = LoadImageFromWebOperations("http://img.rtvslo.si/_up/vreme_2008.png");    
 		vreme.setImageDrawable(drawable);
 	}
 	
+	//funkcija za prikaz vremena s strani rtvslo
 	private Drawable LoadImageFromWebOperations(String url)
 	{
 		try
@@ -48,6 +56,7 @@ public class vrtnar extends Activity implements OnClickListener {
 		}
 	}
 	
+//ko kliknemo se nam odpre novo okno za dodajanje sajenja, hkrat pa doloèamo lahko dneve zalivanja
 	@Override
 	public void onClick(View arg0) {
 
@@ -56,8 +65,74 @@ public class vrtnar extends Activity implements OnClickListener {
 			//this.startActivity(moj);
 			this.startActivityForResult(moj, TEST_START_ACTIVITY_ID);
 		}
+		Intent i = new Intent();
+		i.setClass(this, DneviTeden.class);
+		startActivityForResult(i, OPEN_DNEVI_ZALIVANJA);
+	}
+	
+//izpis za izbran dan ko kliknemo shrani TOAST
+	@Override
+	 protected void onActivityResult (int requestCode, int resultCode, Intent data) {
+		 if (resultCode==RESULT_OK) {
+			 if (requestCode==OPEN_DNEVI_ZALIVANJA) {
+				 String izbrani="";
+				 if (data.getBooleanExtra(DneviZalivanja.zPON, false)) { //neki naredis
+					 izbrani +="PON "; 
+				 }
+				 if (data.getBooleanExtra(DneviZalivanja.zTOR, false)) { //neki naredis
+					 izbrani +="TOR "; 
+				 }
+				 if (data.getBooleanExtra(DneviZalivanja.zSRE, false)) { //neki naredis
+					 izbrani +="SRE "; 
+				 }
+				 if (data.getBooleanExtra(DneviZalivanja.zCET, false)) { //neki naredis
+					 izbrani +="ÈET"; 
+				 }
+				 if (data.getBooleanExtra(DneviZalivanja.zPET, false)) { //neki naredis
+					 izbrani +="PET "; 
+				 }
+				 if (data.getBooleanExtra(DneviZalivanja.zSOB, false)) { //neki naredis
+					 izbrani +="SOB "; 
+				 }
+				 if (data.getBooleanExtra(DneviZalivanja.zNED, false)) { //neki naredis
+					 izbrani +="NET "; 
+				 }
+				 Toast.makeText(this, izbrani, Toast.LENGTH_SHORT)
+					.show(); 
+			 }
+		 }
+	 }
+  
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		meni = menu;
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.menu, meni);
+		return true;
 	}
 
+//odpre se nam novo okno za doloèanje dni zalivanja
+   @Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {			
+		case R.id.btn_dzalivanja:
+			Intent i = new Intent();
+			i.setClass(this, MenuPreferences.class);
+			startActivityForResult(i, R.id.btn_dzalivanja);
+			return true;
+			
+		
+		default:// Generic catch all for all the other menu resources
+			if (!item.hasSubMenu()) {
+				Toast.makeText(this, item.getTitle(), Toast.LENGTH_SHORT)
+				.show();
+				return true;
+			}
+			break;
+		}
+
+		return false;
+	}
 
 
 }
